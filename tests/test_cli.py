2a39107f730a66c32abd2691edb1cd75149d1bdb
Amélie Brun <am.brun@lyon.ort.asso.fr>
2025-04-10 10:03:32 +0200

    Ajout du parametre top dans cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,3 +23,10 @@ def test_top_customers():
     text = string.getvalue()
     assert 'Top' in text
     assert 'customers' in text.lower()
+
+#test pour vérifier la correction du bug
+def test_top_tracks_limit():
+    string = StringIO()
+    cli.top_tracks(top=3, file=string)
+    text = string.getvalue()
+    assert "Top 3 tracks" in text
